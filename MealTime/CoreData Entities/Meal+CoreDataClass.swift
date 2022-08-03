dcd27a428b90fca0f3a8d579964189d2810a5adc
VadislavMarkov1979 <110532140+VadislavMarkov1979@users.noreply.github.com>
2022-08-03 15:38:24 +0400

    Add files via upload

---
+++ MealTime/CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ MealTime/CoreData Entities/Meal+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Meal+CoreDataProperties.swift
+//  MealTime
+//
+
+
+import Foundation
+import CoreData
+
+
+extension Meal {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Meal> {
+        return NSFetchRequest<Meal>(entityName: "Meal")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var user: User?
+
+}
